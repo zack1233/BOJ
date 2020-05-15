c8ab7c1c86fe70336516d9df5bc46d59cdfa3dc7
zack1233 <38316095+zack1233@users.noreply.github.com>
2020-05-15 13:20:53 +0900

    Add files via upload

---
+++ 소스.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main()
+{
+
+	std::cout << "       _.-;;-._ " << std::endl;
+	std::cout << "'-..-'|   ||   |" << std::endl;
+	std::cout << "'-..-'|_.-;;-._|" << std::endl;
+	std::cout << "'-..-'|   ||   |" << std::endl;
+	std::cout << "'-..-'|_.-''-._|";
+
+
+
+	return 0;
+}
