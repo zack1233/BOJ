ceb67b060e3b6996f23bf19394d210d3a8727ad5
zack1233 <38316095+zack1233@users.noreply.github.com>
2020-05-29 13:48:50 +0900

    Add files via upload

---
+++ 소스.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+	std::cout << "    8888888888  888    88888" << std::endl;
+	std::cout << "   88     88   88 88   88  88" << std::endl;
+	std::cout << "    8888  88  88   88  88888" << std::endl;
+	std::cout << "       88 88 888888888 88   88" << std::endl;
+	std::cout << "88888888  88 88     88 88    888888" << std::endl;
+	std::cout << std::endl;
+	std::cout << "88  88  88   888    88888    888888" << std::endl;
+	std::cout << "88  88  88  88 88   88  88  88" << std::endl;
+	std::cout << "88 8888 88 88   88  88888    8888" << std::endl;
+	std::cout << " 888  888 888888888 88  88      88" << std::endl;
+	std::cout << "  88  88  88     88 88   88888888" << std::endl;
+
+	return 0;
+}
