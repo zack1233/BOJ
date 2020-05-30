1bb2925c9dec1d496b7f3c51e427644789bb5763
zack1233 <38316095+zack1233@users.noreply.github.com>
2020-05-30 15:25:02 +0900

    Add files via upload

---
+++ 소스.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+	std::cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE" << std::endl;
+	std::cout << "N2 Bomber      Heavy Fighter  Limited    21        " << std::endl;
+	std::cout << "J-Type 327     Light Combat   Unlimited  1         " << std::endl;
+	std::cout << "NX Cruiser     Medium Fighter Limited    18        " << std::endl;
+	std::cout << "N1 Starfighter Medium Fighter Unlimited  25        " << std::endl;
+	std::cout << "Royal Cruiser  Light Combat   Limited    4         " << std::endl;
+	
+	
+	return 0;
+}
