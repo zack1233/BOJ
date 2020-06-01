3e3d833fce99d3be88262cc536d835734d70bfc3
zack1233 <38316095+zack1233@users.noreply.github.com>
2020-06-01 21:49:22 +0900

    Add files via upload

---
+++ 소스.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+
+	std::cout << "NFC West       W   L  T" << std::endl;
+	std::cout << "-----------------------" << std::endl;
+	std::cout << "Seattle        13  3  0" << std::endl;
+	std::cout << "San Francisco  12  4  0" << std::endl;
+	std::cout << "Arizona        10  6  0" << std::endl;
+	std::cout << "St. Louis      7   9  0" << std::endl;
+	std::cout << std::endl;
+	std::cout << "NFC North      W   L  T" << std::endl;
+	std::cout << "-----------------------" << std::endl;
+	std::cout << "Green Bay      8   7  1" << std::endl;
+	std::cout << "Chicago        8   8  0" << std::endl;
+	std::cout << "Detroit        7   9  0" << std::endl;
+	std::cout << "Minnesota      5  10  1" << std::endl;
+
+
+
+
+	return 0;
+}
