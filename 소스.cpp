dd378393d31fc23e1da0d60b62ad799e7152ccb7
zack1233 <38316095+zack1233@users.noreply.github.com>
2020-06-02 10:31:07 +0900

    Add files via upload

---
+++ 소스.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+	std::cout << "\\    /\\" << std::endl;
+	std::cout << " )  ( ')" << std::endl;
+	std::cout << "(  /  )" << std::endl;
+	std::cout << " \\(__)|";
+
+
+	return 0;
+}
