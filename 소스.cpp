b1c0c7dd75f14b4a3a24dbddccea45f75479902f
zack1233 <38316095+zack1233@users.noreply.github.com>
2020-06-09 16:25:03 +0900

    Add files via upload

---
+++ 소스.cpp
@@ -0,0 +1,36 @@
+#pragma warning(disable:4996)
+#include <iostream>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+
+	struct tm* t;
+	string s = "";
+	time_t timer;
+	timer = time(NULL);
+
+	t = localtime(&timer);
+	s += to_string(t->tm_year + 1900);
+	s += "-";
+
+	if (t->tm_mon + 1 < 10) {
+		s += "0";
+	}
+
+	s += to_string(t->tm_mon + 1);
+	s += "-";
+
+	if (t->tm_mday + 1 < 10) {
+		s += "0";
+	}
+	
+	s += to_string(t->tm_mday);
+
+	cout << s;
+
+
+	return 0;
+}
